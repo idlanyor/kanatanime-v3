ac5e30faf68545b9746411c3a30155473134af19
idlanyor <prasbhara0604@gmail.com>
2026-02-28 21:59:30 +0700

    ganti endpoint

--- server/scraper.ts
+++ server/scraper.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-const BASE_URL = 'https://v5.animekompi.fun';
+const BASE_URL = 'https://v6.animekompi.fun';
 
 const headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
